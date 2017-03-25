3f449b77ca8e89a6b54364f38b3b2d2747dd95e2
Dave Pearson <davep@davep.org>
2017-03-25 13:43:23 +0000

    v1.4

--- unbind.el
+++ unbind.el
@@ -2,7 +2,7 @@
 ;; Copyright 2002-2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.3
+;; Version: 1.4
 ;; Keywords: lisp, unbind
 ;; URL: https://github.com/davep/unbind.el
 ;; Package-Requires: ((cl-lib "0.5"))
